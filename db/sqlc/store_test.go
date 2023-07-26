7aa4cd4ceb8265705f9a09c68e6b25f9e2cf1e95
MD Mobin <74850678+Djsmk123@users.noreply.github.com>
2023-07-26 18:44:01 +0530

    added deadlock avoid

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -118,3 +118,55 @@ func TestStore(t *testing.T) {
 	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
 
 }
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	//run n concurrent operations
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+		if i%2 == 0 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+
+			__, err := store.Transferx(context.Background(), TransfersTxParams{
+				FromAccountId: fromAccountID,
+				ToAccountId:   toAccountID,
+				Amount:        amount,
+			})
+			fmt.Println(__)
+			errs <- err
+
+		}()
+
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+	}
+
+	//check the final updated amount
+
+	// check the final updated balance
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
